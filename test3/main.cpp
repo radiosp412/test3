c8d2fb05cbee90762d6f5671514769915f390fc2
Stephen Pickup <pickup@pennmedicine.upenn.edu>
2022-08-30 13:33:28 -0400

    Initial Commit

---
+++ test3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test3
+//
+//  Created by Stephen Pickup on 8/30/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
